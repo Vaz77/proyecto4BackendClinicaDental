5208805093c07d2cc880d89e00f1dac5711a7eea
Jesus <jesusvazquez2477@gmail.com>
2023-06-09 20:55:47 +0200

    Añado modelo role, realizo migracion y creo usuarios

---
+++ seeders/20230609183353-roleSeeder.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**@type {import('sequelize-cli').Migration}*/
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert ('Roles', [
+
+      {id: 1,
+        name: "Admin",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {id: 2,
+        name: "Doctor",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {id: 3,
+        name: "Patient",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+
+    ], {});
+
+  },
+
+  async down (queryInterface, Sequelize) {
+
+}
+};
