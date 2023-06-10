1834c3f74308d91a8f1b96ddc9d0f3b850fcdebe
Jesus <jesusvazquez2477@gmail.com>
2023-06-10 08:47:20 +0200

    Conecto base de datos, añado promesas para que nos avise en caso de error,

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
-
+const db = require('./db');
 const app = express();
 
 
 
 const PORT = 3000;
 
-app.use(express.json());
-app.listen(PORT, () =>{
-    console.log("Servidor levantado en el puerto" + PORT);
-})
+app.get('/health', (req, res) => {
+    return res.send('healthy');
+});
+
+db.then(() =>
+    {
+        app.listen(PORT, () => {
+            console.log('Server is running on port: ' + PORT);
+        })
+    }
+).catch((error) => {
+    console.error('Error starting server', error.message);
+});
